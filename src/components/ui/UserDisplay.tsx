ea4b59f45d392d5ab6864586f1657e3f4025877a
Ambros Eberhard <ambros.eberhard@gmx.ch>
2024-03-06 08:27:27 +0100

    created files for user inspection/editing

---
+++ src/components/ui/UserDisplay.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import BaseContainer from "./BaseContainer";
+
+
+
+
+function UserDisplay(props){
+
+
+    return(
+        <BaseContainer>
+
+            {props.name ? <div>Hello {props.name} welcome back</div> : null}
+            <div>`ID:  ${props.id}`</div>
+            <div>`Username:  ${props.username}`</div>
+            <div>`Creation date:  ${props.creationTime}`</div>
+            <div>`ID  ${props.id}`</div>
+            <div>`ID  ${props.id}`</div>
+
+        </BaseContainer>
+    )
+
+}
+
+
+export default userDisplay;
---
+++ src/components/views/UserEdit.tsx
@@ -0,0 +1,16 @@
+import { Button } from "components/ui/Button";
+import React, {useState} from "react";
+import BaseContainer from "../ui/BaseContainer";
+
+
+
+
+function UserEdit(){
+
+    return(
+        <BaseContainer>
+
+        </BaseContainer>
+    )
+
+}
